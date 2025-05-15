96e9b7860bb70acab2a1cb4bff78ac7e2ab72992
Seka Shalom <sekashalom74@gmail.com>
2025-05-15 21:47:10 +0200

    Initial commit for best timer

---
+++ script1.js
@@ -0,0 +1,9 @@
+const button=document.getElementById('startButton');
+button.addEventListener('click' ,()=>{
+    const div=document.getElementById("div1")
+    div.style.display="none"
+    button.style.display="none"
+    const area=document.getElementById("countdownArea")
+    area.style.display="flex"
+
+})
