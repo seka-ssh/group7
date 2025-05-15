7cc3b84f04718239d575043669c7a53f2de33018
Seka Shalom <sekashalom74@gmail.com>
2025-05-15 22:15:27 +0200

    Deploing best timer

--- script1.js
+++
@@ -1,9 +0,0 @@
-const button=document.getElementById('startButton');
-button.addEventListener('click' ,()=>{
-    const div=document.getElementById("div1")
-    div.style.display="none"
-    button.style.display="none"
-    const area=document.getElementById("countdownArea")
-    area.style.display="flex"
-
-})
